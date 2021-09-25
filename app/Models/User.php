f1ae1427776ef173781684ccd32c3780d54aea41
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-09-25 09:32:28 +0700

    setter for password

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -27,7 +28,8 @@ class User extends Authenticatable
         'email',
         'password',
         'customer_id',
-        'role'
+        'role',
+        'active'
     ];
 
     /**
@@ -57,5 +59,10 @@ class User extends Authenticatable
     public function isUser()
     {
         return $this->role == self::ROLE_USER;
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
     }
 }
