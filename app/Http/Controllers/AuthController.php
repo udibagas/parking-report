52a1ea1c2729b1d70572401931cd184c10fdfac9
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-09-25 09:32:01 +0700

    add methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $request->validate(['email' => 'required', 'password' => 'required']);
+
+        $user = User::where('name', $request->email)
+            ->orWhere('email', $request->email)
+            ->first();
+
+        if (!$user) {
+            return response(['message' => 'User tidak terdaftar'], 404);
+        }
+
+        if (!$user->active) {
+            return response(['message' => 'User tidak aktif. Silakan hubungi Admin.'], 401);
+        }
+
+        if (password_verify($request->password, $user->password)) {
+            Auth::login($user, true);
+
+            return response()->json([
+                'token' => $user->createToken($request->device_name ?: 'web')->plainTextToken,
+                'user' => $user
+            ]);
+        }
+
+        return response(['message' => 'Mohon periksa kembali password Anda.']);
+    }
+
+
+    public function logout()
+    {
+        return response('', 204);
+    }
+
+    public function me()
+    {
+        return ['user' => auth()->user()];
+    }
 }
