29f87fd1607bdcf37d45eed41959d64e02bca1ee
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-09-25 09:33:32 +0700

    make seeder for admin user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'password' => 'admin123',
+            'email' => 'admin@mail.com',
+            'role' => 1,
+            'active' => true
+        ]);
+    }
+}
