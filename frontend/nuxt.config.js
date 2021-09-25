379fdc35572f3f142751e8cf3b1eccc72d5a1e9d
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-09-25 10:06:44 +0700

    set config for some library

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -9,29 +9,25 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - frontend',
-    title: 'frontend',
+    titleTemplate: '%s - MitraTeknik Parking Report',
+    title: 'MitraTeknik Parking Report',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: ['@/plugins/decimal'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -40,16 +36,42 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
+    '@nuxtjs/moment',
+    '@nuxtjs/composition-api/module',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
+    '@nuxtjs/auth-next',
     '@nuxtjs/axios',
   ],
 
+  router: {
+    mode: 'hash',
+  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    credentials: true,
+    // development
+    baseURL: process.env.API_URL || 'http://localhost:8000',
+    // production
+    // baseURL: process.env.API_URL || '',
+  },
+
+  auth: {
+    strategies: {
+      cookie: {
+        endpoints: {
+          login: { url: '/api/login' },
+          logout: { url: '/api/logout' },
+          user: { url: '/api/me' },
+          csrf: { url: '/sanctum/csrf-cookie' },
+        },
+      },
+    },
+  },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
@@ -64,13 +86,12 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
