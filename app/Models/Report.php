7bfb56c924e87ec23cfae80c7a4cdee3099b79bb
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-09-25 09:32:14 +0700

    add relation to customer

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     use HasFactory;
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
