447c930ea98cbeb3a8392f5aa36421b70e424c64
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-09-27 08:52:57 +0700

    add customer information

--- frontend/pages/my-account.vue
+++ frontend/pages/my-account.vue
@@ -1,54 +1,92 @@
 <template>
-	<v-card :loading="loading" flat>
-		<v-card-title> My Account </v-card-title>
+	<div>
+		<div class="text-h6 ml-2 mb-3">My Account</div>
+		<v-card :loading="loading" flat>
+			<v-card-text>
+				<v-form ref="form" lazy-validation>
+					<v-subheader class="mb-4">
+						<v-icon left>mdi-account-circle-outline</v-icon>
+						Informasi User
+					</v-subheader>
 
-		<v-divider></v-divider>
+					<v-text-field
+						outlined
+						dense
+						v-model="form.name"
+						label="Nama"
+					></v-text-field>
 
-		<v-card-text>
-			<v-form ref="form" lazy-validation>
-				<v-text-field
-					outlined
-					dense
-					v-model="form.name"
-					label="Nama"
-				></v-text-field>
+					<v-text-field
+						outlined
+						dense
+						v-model="form.email"
+						label="Email"
+					></v-text-field>
 
-				<v-text-field
-					outlined
-					dense
-					v-model="form.email"
-					label="Email"
-				></v-text-field>
+					<v-text-field
+						outlined
+						dense
+						v-model="form.password"
+						type="password"
+						label="Password"
+					></v-text-field>
 
-				<v-text-field
-					outlined
-					dense
-					v-model="form.password"
-					type="password"
-					label="Password"
-				></v-text-field>
+					<v-text-field
+						outlined
+						dense
+						v-model="form.password_confirmation"
+						type="password"
+						label="Konfirmasi Password"
+					></v-text-field>
 
-				<v-text-field
-					outlined
-					dense
-					v-model="form.password_confirmation"
-					type="password"
-					label="Konfirmasi Password"
-				></v-text-field>
+					<v-subheader v-if="form.customer" class="mb-4">
+						<v-icon left>mdi-badge-account-horizontal-outline</v-icon>
+						Informasi Pelanggan
+					</v-subheader>
 
-				<v-btn color="primary" block elevation="0" large @click="save">
-					SIMPAN
-				</v-btn>
-			</v-form>
-		</v-card-text>
-	</v-card>
+					<v-text-field
+						v-if="form.customer"
+						outlined
+						dense
+						v-model="form.customer.nama"
+						label="Nama Pelanggan"
+					></v-text-field>
+
+					<v-textarea
+						v-if="form.customer"
+						outlined
+						dense
+						v-model="form.customer.alamat"
+						label="Alamat"
+						rows="2"
+						auto-grow
+					></v-textarea>
+
+					<v-textarea
+						v-if="form.customer"
+						readonly
+						outlined
+						dense
+						auto-grow
+						rows="2"
+						v-model="form.customer.id"
+						label="ID Pelanggan"
+					></v-textarea>
+
+					<v-btn color="primary" block elevation="0" large @click="save">
+						SIMPAN
+					</v-btn>
+				</v-form>
+			</v-card-text>
+		</v-card>
+	</div>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			form: { ...this.$auth.user },
+			form: JSON.parse(JSON.stringify(this.$auth.user)),
 			rules: {},
 			loading: false,
 		}
@@ -61,10 +99,10 @@ export default {
 			this.$axios
 				.$put(`/api/user/${this.form.id}`, this.form)
 				.then((r) => {
-					// this.$notifier.showMessage({
-					// 	content: r.data.message,
-					// 	color: 'success',
-					// })
+					this.$notifier.showMessage({
+						content: r.data.message,
+						color: 'success',
+					})
 				})
 				.catch((e) => {
 					if (e.response.status == 422) {
