a5397dae029cdc0b4b69ba4f99597ac3c66c246e
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-09-27 08:15:08 +0700

    remove duplicate entry

--- routes/api.php
+++ routes/api.php
@@ -25,12 +25,9 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('report', [ReportController::class, 'store']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    // auth related
     Route::get('me', [AuthController::class, 'me']);
     Route::post('logout', [AuthController::class, 'logout']);
-
     Route::get('report', [ReportController::class, 'index']);
-    Route::post('report', [ReportController::class, 'store']);
 
     Route::apiResources([
         'user' => UserController::class,
